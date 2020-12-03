d58f4b0972c0eef2f2438e833ee5af2622e129a6
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-12-04 01:59:11 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = (props) => {
 
-  // const initialStates = {
-  //   name: '',
-  //   price: 1000
-  // }
-
   const [state, setState] = useState(props); 
   const {name, price} = state
-  // const [name, setName] = useState(initialStates.name);
-  // const [price, setPrice] = useState(initialStates.price);
 
-  // const reset = () => {
-  //   setState(props)
-  //   // setPrice(initialStates.price)
-  //   // setName(initialStates.name)
-  // }
+  useEffect(() => {
+    console.log('This is a componentDidMount or componentDidUpdate')
+  })
+
+  useEffect(() => {
+    console.log('This is a componentDidMount')
+  }, [])
+
+  useEffect(() => {
+    console.log('This callback is for name only')
+  }, [name])
 
   return (
     <>
